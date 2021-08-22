6ba048abb23d65c6c75a0c63f603c68a0f17a198
jk <1097820454@qq.com>
2021-08-22 18:03:16 +0800

    Second Commit

--- src/cn/edu/hhu/HelloGit.java
+++ src/cn/edu/hhu/HelloGit.java
@@ -3,5 +3,6 @@ package cn.edu.hhu;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git！Hello World！");
+        System.out.println("This message test fro pull/push");
     }
 }
