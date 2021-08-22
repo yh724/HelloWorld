8f379837a31ab12b1b37dfd16b5d415da0a647a9
jk <1097820454@qq.com>
2021-08-22 18:24:11 +0800

    123

--- src/cn/edu/hhu/HelloGit.java
+++ src/cn/edu/hhu/HelloGit.java
@@ -3,7 +3,8 @@ package cn.edu.hhu;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git！Hello World！");
-        System.out.println("This message test fro pull/push");
+        System.out.println("This message test for pull/push");
         System.out.println("Sometimes you have to let things go so there’s room for better things to come into your life.");
+        System.out.println("This message test for clash.[two]");
     }
 }
